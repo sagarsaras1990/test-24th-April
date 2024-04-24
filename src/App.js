b91b9a9fad20a251b5593fbec7165e7d6938f985
Sagar Verma <153511566+sagarsaras1990@users.noreply.github.com>
2024-04-24 19:27:59 +0530

    Initial msg

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './App.css'
 function App() {
+  const [pokemonList, setPokemonList] = useState([]);
+  const [selectedPokemon, setSelectedPokemon] = useState(null);
+  const [relatedimg,setRelatedImg]= useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://pokeapi.co/api/v2/pokemon/');
+        setPokemonList(response.data.results);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const handlePokemonClick = async (pokemon) => {
+    try {
+      const response = await axios.get(pokemon.url);
+      setSelectedPokemon(response.data);
+
+    } catch (error) {
+      console.error('Error fetching Pokemon details:', error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>    <div className="App row">
+      <div className="pokemon-list col">
+        <h2 className='pkm'>Pokemon List</h2>
+        
+        <table className="table">
+          <tbody>
+            {pokemonList.map((pokemon, index) => (
+              <tr>
+                <td> </td>
+             <td key={index} onClick={() => handlePokemonClick(pokemon)}>{pokemon.name}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+
+      </div>
+      <div className="pokemon-details col">
+        <h2 className='pkm'>Pokemon Details</h2>
+        {selectedPokemon && (
+          <div>
+            <img src={selectedPokemon.sprites.front_default} alt={selectedPokemon.name} />
+            <table className="table">
+              <tbody>
+                <tr>
+                  <td><b>Name</b></td>
+                  <td>{selectedPokemon.name}</td>
+                </tr>
+                <tr>
+                  <td><b>Height</b></td>
+                  <td>{selectedPokemon.height}</td>
+                </tr>
+                <tr>
+                  <td><b>Weight</b></td>
+                  <td>{selectedPokemon.weight}</td>
+                </tr>
+                <tr>
+                  <td><b>Types</b></td>
+                  <td>{selectedPokemon?.types[1]?.type?.name}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        )}
+      </div>
     </div>
+    </>
+
   );
 }
 
 export default App;
+
